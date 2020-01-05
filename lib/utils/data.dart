3958408e2637a2e058eccfd1a90dae151a0bd6b4
Aby Mathew <vabym8@gmail.com>
2020-01-05 12:46:27 +0300

    Create data.dart

---
+++ lib/utils/data.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+var kActiveTabTextStyle = GoogleFonts.acme(
+  textStyle: TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+    fontSize: 18.0,
+  ),
+);
+
+var kInActiveTabTextStyle = GoogleFonts.acme(
+  textStyle: TextStyle(
+    color: Colors.grey[300],
+    fontWeight: FontWeight.bold,
+    fontSize: 18.0,
+  ),
+);
+
+List<String> imgUrl = [
+  'https://i.picsum.photos/id/377/200/300.jpg',
+  'https://i.picsum.photos/id/276/200/300.jpg',
+  'https://i.picsum.photos/id/72/200/300.jpg',
+  'https://i.picsum.photos/id/329/200/300.jpg',
+  'https://i.picsum.photos/id/1000/200/300.jpg',
+  'https://i.picsum.photos/id/512/200/300.jpg',
+  'https://i.picsum.photos/id/393/200/300.jpg',
+  'https://i.picsum.photos/id/329/200/300.jpg',
+];
